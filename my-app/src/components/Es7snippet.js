b66c1d4e5f38f564d5439fb5cd54d6d1954499e9
aimansae <saeedaimann@gmail.com>
2023-01-05 20:51:51 +0000

    add ES7 Snippets Cheat Sheet

---
+++ my-app/src/components/Es7snippet.js
@@ -0,0 +1,52 @@
+// rfce
+
+// import React from 'react'
+
+// function Es7snippet() {
+//   return (
+//     <div>Es7snippet</div>
+//   )
+// }
+
+// export default Es7snippet
+
+
+// rafce with arrow fucntion
+// import React from 'react'
+
+// const Es7snippet = () => {
+//   return (
+//     <div>Es7snippet</div>
+//   )
+// }
+
+// export default Es7snippet
+
+
+// rce class componet
+// import React, { Component } from 'react'
+
+// export class Es7snippet extends Component {
+//   render() {
+//     return (
+//       <div>Es7snippet</div>
+//     )
+//   }
+// }
+
+// export default Es7snippet
+
+
+//rconst with contructor
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// }
+
+//clg onsole long
+console.log(first)
+// clo to console with nale
+console.log('first', first)
